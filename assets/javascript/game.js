55db551740f115e67b5f9a6a4b99f9e44050e8d5
Nick Gluch <nickrgluch@yahoo.com>
2019-06-24 22:10:02 -0500

    clicks

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+$(document).ready()
+/*var randomStart = $('#RandomNumber')
+{
+    Math.floor(Math.random() * 101);
+
+};*/
+var randomStart =
+    $('#RandomNumber').html(Math.floor(Math.random() * 101));
+
+$('#ruby').click(function () {
+    alert("+5");
+});
+
+
+$('#bluegem').click(function () {
+    alert("+7");
+});
+
+
+$('#yellowgem').click(function () {
+    alert("+10");
+});
+
+
+$('#emerald').click(function () {
+    alert("+3");
+});
+
+
