71c1d7433c671209bf24dc3012b27d2855577791
Nick Gluch <nickrgluch@yahoo.com>
2019-06-26 18:20:51 -0500

    up to game restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,70 @@
 
-$(document).ready()
-/*var randomStart = $('#RandomNumber')
-{
-    Math.floor(Math.random() * 101);
+$(document).ready(function () {
 
-};*/
-var randomStart =
-    $('#RandomNumber').html(Math.floor(Math.random() * 101));
+    //    $(window).onload(function game() {
 
-$('#ruby').click(function () {
-    alert("+5");
+    var randomNumber = Math.floor(Math.random() * 101)
+    $('#RandomNumber').html(randomNumber);
+    //var currentScore = 0;
+    var win = 0;
+    var lose = 0;
+
+
+
+    var currentScore = 0;
+    var currentScoreElement = $('#totalScoreNumber');
+    currentScoreElement.html(0);
+
+
+    function checkWin() {
+        if (currentScore === randomNumber) {
+            alert('You won!')
+            win++;
+            $("#wins").text('Wins: ' + win);
+            game();
+            console.log(win);
+
+        }
+
+        if (currentScore > randomNumber) {
+            alert('Lost!');
+            lose++;
+            $("#losses").text('Losses: ' + lose);
+            game();
+        }
+    }
+
+    $('#ruby').click(function () {
+        currentScore += 5;
+        currentScoreElement.html(currentScore);
+        checkWin()
+
+        //evalutate win or loss condition
+        //if match then win
+        //once past number loss
+
+    });
+
+
+    $('#bluegem').click(function () {
+        currentScore += 3;
+        currentScoreElement.html(currentScore);
+        checkWin()
+    });
+
+    $('#yellowgem').click(function () {
+        currentScore += 7;
+        currentScoreElement.html(currentScore);
+        checkWin()
+    });
+
+
+    $('#emerald').click(function () {
+        currentScore += 4;
+        currentScoreElement.html(currentScore);
+        checkWin()
+    });
+
+
+    //end of entire function
 });
-
-
-$('#bluegem').click(function () {
-    alert("+7");
-});
-
-
-$('#yellowgem').click(function () {
-    alert("+10");
-});
-
-
-$('#emerald').click(function () {
-    alert("+3");
-});
-
-
